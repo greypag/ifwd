2ae77dc5ee19fe0e144ee21e72dfbc9c7cc339f8
Nat Cheung <nathaniel.kw.cheung@fwd.com>
2015-04-08 11:02:45 +0800

    Update: Add new utils for mapping the name

--- src/main/java/com/ifwd/fwdhk/util/WebServiceUtils.java
+++ src/main/java/com/ifwd/fwdhk/util/WebServiceUtils.java
@@ -1,7 +1,13 @@
 package com.ifwd.fwdhk.util;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
+import com.ifwd.fwdhk.model.DistrictBean;
 
 public class WebServiceUtils {
 	public static String transformLanaguage(String language) {
@@ -10,16 +16,44 @@ public class WebServiceUtils {
 		else
 			return "EN";
 	}
-	
-	public static String getParameterValue(String key, HttpSession session, HttpServletRequest request) {
-		
+
+	public static String getParameterValue(String key, HttpSession session,
+			HttpServletRequest request) {
+
 		if (request.getParameter(key) == null) {
-			return (String)session.getAttribute(key);
-		}
-		else {
-			System.out.println("SET VALUE KEY " + key + " VALUE " + request.getParameter(key));
+			return (String) session.getAttribute(key);
+		} else {
+			System.out.println("SET VALUE KEY " + key + " VALUE "
+					+ request.getParameter(key));
 			session.setAttribute(key, request.getParameter(key));
 			return request.getParameter(key);
 		}
 	}
+
+	public static String getDistrictNameFromMap(
+			List<DistrictBean> districtList, String districtCode) {
+		Iterator<DistrictBean> iterator = districtList.iterator();
+		String name = "";
+		while (iterator.hasNext()) {
+			DistrictBean districtBean = iterator.next();
+			if (districtBean.getCode() == districtCode) {
+				name = districtBean.getDescription();
+				break;
+			}
+		}
+		return name;
+	}
+
+	public static String getNameFromMap(Map<String, String> map,
+			String key) {
+		String name = "";
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+			if (entry.getKey() == key) {
+				name = entry.getValue();
+				break;
+			}			
+		}
+		return name;
+	}
+
 }
