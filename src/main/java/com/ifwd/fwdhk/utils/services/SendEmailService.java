f7293c57eb74d6f29e20f2b6f61c85e0f40d18bb
Nat Cheung <nathaniel.kw.cheung@fwd.com>
2015-05-11 19:51:55 +0800

    UTF8 Email

--- src/main/java/com/ifwd/fwdhk/utils/services/SendEmailService.java
+++ src/main/java/com/ifwd/fwdhk/utils/services/SendEmailService.java
@@ -27,12 +27,12 @@ public class SendEmailService implements SendEmailDao {
 //		String message = "<p>Thank your for registering for receiving offers, upgrades and information from FWD*.  Your promotion code is ABC. <br><p>"
 //				+ "FWD respects your privacy. For more information, please read our privacy policy at <a href='http://www.fwd.com.hk/en-US/home/pdo.html'>http://www.fwd.com.hk/en-US/home/pdo.html</a>.<br><p>"
 //				+ "*Note: You may unsubscribe at any time.  To unsubscribe, pls call us at 3123-3123.<br><br><br>";
-//		String message = "\u60A8\u597D,<p><p>\u591A\u8B1D\u60A8\u767B\u8A18\u8A02\u95B1\u5BCC\u885B\u7684\u63A8\u5EE3\u512A\u60E0\u3001\u5347\u7D1A\u734E\u8CDE\u53CA\u516C\u53F8\u8CC7\u8A0A*\u3002\u60A8\u53EF\u4EE5\u4F7F\u7528\u63A8\u5EE3\u7DE8\u865F<JOHNDO> \u7372\u53D6\u7DB2\u4E0A\u6295\u4FDD\u512A\u60E0\u3002<p>"
-//				+ "\u5BCC\u885B\u6642\u523B\u4EE5\u5BA2\u6236\u79C1\u96B1\u70BA\u5148\uFF0C\u4F60\u53EF\u4EE5\u53C3\u95B1<a href='http://www.fwd.com.hk/zh-HK/home/pdo.html'>http://www.fwd.com.hk/zh-HK/home/pdo.html</a>\u4E86\u89E3\u6211\u5011\u7684\u500B\u4EBA\u8CC7\u6599\u4FDD\u8B77\u653F\u7B56\u3002<p>*\u8A3B: \u60A8\u53EF\u4EE5\u96A8\u6642\u81F4\u96FB3123-3123\u53D6\u6D88\u8A02\u95B1<p><p>Hi,<p><p>Thank you for registering to receive offers, upgrades and information from FWD*. You can use the promotion code <JOHNDO> to get discounts from online purchases.<p>FWD respects your privacy. For more information, please read our Personal Data Protection Policy at <a href='http://www.fwd.com.hk/en-US/home/pdo.html'>http://www.fwd.com.hk/en-US/home/pdo.html</a>.<p> *Note: You may unsubscribe at any time.  To unsubscribe, please call us at 3123-3123.";
+		String message = "\u60A8\u597D,<p><p>\u591A\u8B1D\u60A8\u767B\u8A18\u8A02\u95B1\u5BCC\u885B\u7684\u63A8\u5EE3\u512A\u60E0\u3001\u5347\u7D1A\u734E\u8CDE\u53CA\u516C\u53F8\u8CC7\u8A0A*\u3002\u60A8\u53EF\u4EE5\u4F7F\u7528\u63A8\u5EE3\u7DE8\u865F<JOHNDO> \u7372\u53D6\u7DB2\u4E0A\u6295\u4FDD\u512A\u60E0\u3002<p>"
+				+ "\u5BCC\u885B\u6642\u523B\u4EE5\u5BA2\u6236\u79C1\u96B1\u70BA\u5148\uFF0C\u4F60\u53EF\u4EE5\u53C3\u95B1<a href='http://www.fwd.com.hk/zh-HK/home/pdo.html'>http://www.fwd.com.hk/zh-HK/home/pdo.html</a>\u4E86\u89E3\u6211\u5011\u7684\u500B\u4EBA\u8CC7\u6599\u4FDD\u8B77\u653F\u7B56\u3002<p>*\u8A3B: \u60A8\u53EF\u4EE5\u96A8\u6642\u81F4\u96FB3123-3123\u53D6\u6D88\u8A02\u95B1<p><p>Hi,<p><p>Thank you for registering to receive offers, upgrades and information from FWD*. You can use the promotion code <JOHNDO> to get discounts from online purchases.<p>FWD respects your privacy. For more information, please read our Personal Data Protection Policy at <a href='http://www.fwd.com.hk/en-US/home/pdo.html'>http://www.fwd.com.hk/en-US/home/pdo.html</a>.<p> *Note: You may unsubscribe at any time.  To unsubscribe, please call us at 3123-3123.";
 //		
 		//ByteBuffer message = Charset.forName("UTF-8").encode("您好");
 		
-		String message = "\u60A8\u597D";
+		//String message = "\u60A8\u597D";
 		JSONObject email_params = new JSONObject();
 		email_params.put("to", emailId);
 		email_params.put("message", message);
