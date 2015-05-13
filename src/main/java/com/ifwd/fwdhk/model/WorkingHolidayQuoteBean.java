17b7236e869d57482fb24dd4362d794ca4c8164d
Gavin <Gavin.B.Li@pccw.com>
2015-05-13 14:45:53 +0800

    add model

---
+++ src/main/java/com/ifwd/fwdhk/model/WorkingHolidayQuoteBean.java
@@ -0,0 +1,101 @@
+package com.ifwd.fwdhk.model;
+
+public class WorkingHolidayQuoteBean {
+	
+	private String trLeavingDate;
+	private String trBackDate;
+	private int totalWorkingHolidayer;
+	private boolean personalPlan ;
+	private boolean familyPlan ;
+	private int totalPersonalWorkingHolidayer;
+	private int totalAdultWorkingHolidayer;
+	private int totalChildWorkingHolidayer;
+	private int totalOtherWorkingHolidayer;
+	private int totalWorkingHolidayingDays;
+	private String planSelected;
+	private boolean selfCover;
+	private boolean spouseCover;
+	public String getTrLeavingDate() {
+		return trLeavingDate;
+	}
+	public void setTrLeavingDate(String trLeavingDate) {
+		this.trLeavingDate = trLeavingDate;
+	}
+	public String getTrBackDate() {
+		return trBackDate;
+	}
+	public void setTrBackDate(String trBackDate) {
+		this.trBackDate = trBackDate;
+	}
+	public int getTotalWorkingHolidayer() {
+		return totalWorkingHolidayer;
+	}
+	public void setTotalWorkingHolidayer(int totalWorkingHolidayer) {
+		this.totalWorkingHolidayer = totalWorkingHolidayer;
+	}
+	public boolean isPersonalPlan() {
+		return personalPlan;
+	}
+	public void setPersonalPlan(boolean personalPlan) {
+		this.personalPlan = personalPlan;
+	}
+	public boolean isFamilyPlan() {
+		return familyPlan;
+	}
+	public void setFamilyPlan(boolean familyPlan) {
+		this.familyPlan = familyPlan;
+	}
+	public int getTotalPersonalWorkingHolidayer() {
+		return totalPersonalWorkingHolidayer;
+	}
+	public void setTotalPersonalWorkingHolidayer(int totalPersonalWorkingHolidayer) {
+		this.totalPersonalWorkingHolidayer = totalPersonalWorkingHolidayer;
+	}
+	public int getTotalAdultWorkingHolidayer() {
+		return totalAdultWorkingHolidayer;
+	}
+	public void setTotalAdultWorkingHolidayer(int totalAdultWorkingHolidayer) {
+		this.totalAdultWorkingHolidayer = totalAdultWorkingHolidayer;
+	}
+	public int getTotalChildWorkingHolidayer() {
+		return totalChildWorkingHolidayer;
+	}
+	public void setTotalChildWorkingHolidayer(int totalChildWorkingHolidayer) {
+		this.totalChildWorkingHolidayer = totalChildWorkingHolidayer;
+	}
+	public int getTotalOtherWorkingHolidayer() {
+		return totalOtherWorkingHolidayer;
+	}
+	public void setTotalOtherWorkingHolidayer(int totalOtherWorkingHolidayer) {
+		this.totalOtherWorkingHolidayer = totalOtherWorkingHolidayer;
+	}
+	public int getTotalWorkingHolidayingDays() {
+		return totalWorkingHolidayingDays;
+	}
+	public void setTotalWorkingHolidayingDays(int totalWorkingHolidayingDays) {
+		this.totalWorkingHolidayingDays = totalWorkingHolidayingDays;
+	}
+	public String getPlanSelected() {
+		return planSelected;
+	}
+	public void setPlanSelected(String planSelected) {
+		this.planSelected = planSelected;
+	}
+	public boolean isSelfCover() {
+		return selfCover;
+	}
+	public void setSelfCover(boolean selfCover) {
+		this.selfCover = selfCover;
+	}
+	public boolean isSpouseCover() {
+		return spouseCover;
+	}
+	public void setSpouseCover(boolean spouseCover) {
+		this.spouseCover = spouseCover;
+	}
+	
+	
+	
+
+	
+}
