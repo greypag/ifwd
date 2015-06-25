2a717270e92787c694f593a55dbc7cd5c2ab8f28
Jerry Wu <Jerry.JM.Wu@PCCW.COM>
2015-06-25 17:17:18 +0800

    ok

---
+++ src/main/java/com/ifwd/fwdhk/model/SavieBeneficiaryBean.java
@@ -0,0 +1,68 @@
+package com.ifwd.fwdhk.model;
+
+public class SavieBeneficiaryBean {
+	
+	
+	private String firstName;
+	private String lastName;
+	private String chineseName;
+	private String hkid;
+	private String passport;
+	private String gender;
+	private String relationship;
+	private String entitlement;
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getChineseName() {
+		return chineseName;
+	}
+	public void setChineseName(String chineseName) {
+		this.chineseName = chineseName;
+	}
+	public String getHkid() {
+		return hkid;
+	}
+	public void setHkid(String hkid) {
+		this.hkid = hkid;
+	}
+	public String getPassport() {
+		return passport;
+	}
+	public void setPassport(String passport) {
+		this.passport = passport;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getRelationship() {
+		return relationship;
+	}
+	public void setRelationship(String relationship) {
+		this.relationship = relationship;
+	}
+	public String getEntitlement() {
+		return entitlement;
+	}
+	public void setEntitlement(String entitlement) {
+		this.entitlement = entitlement;
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/com/ifwd/fwdhk/model/SaviePersonalBean.java
@@ -0,0 +1,146 @@
+package com.ifwd.fwdhk.model;
+
+import java.util.Date;
+
+
+
+public class SaviePersonalBean {
+	private String chineseName;
+	private String englishName;	
+	private String HKID;		
+	private String gender;
+	private String maritalStatus;	
+	private Date dateOfBirth;
+	private String placeOfBirth;
+	private String nationality;
+	private String email;
+	private String telephone1;
+	private String telephone2;
+	private String telephone3;
+	private String mobile1;
+	private String mobile2;
+	private String mobile3;
+	private String address1;	
+	private String address2;	
+	private String address3;	
+	private String district;
+	
+	
+	public String getChineseName() {
+		return chineseName;
+	}
+	public void setChineseName(String chineseName) {
+		this.chineseName = chineseName;
+	}
+	public String getEnglishName() {
+		return englishName;
+	}
+	public void setEnglishName(String englishName) {
+		this.englishName = englishName;
+	}
+	public String getHKID() {
+		return HKID;
+	}
+	public void setHKID(String hKID) {
+		HKID = hKID;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getMaritalStatus() {
+		return maritalStatus;
+	}
+	public void setMaritalStatus(String maritalStatus) {
+		this.maritalStatus = maritalStatus;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getPlaceOfBirth() {
+		return placeOfBirth;
+	}
+	public void setPlaceOfBirth(String placeOfBirth) {
+		this.placeOfBirth = placeOfBirth;
+	}
+	public String getNationality() {
+		return nationality;
+	}
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelephone1() {
+		return telephone1;
+	}
+	public void setTelephone1(String telephone1) {
+		this.telephone1 = telephone1;
+	}
+	public String getTelephone2() {
+		return telephone2;
+	}
+	public void setTelephone2(String telephone2) {
+		this.telephone2 = telephone2;
+	}
+	public String getTelephone3() {
+		return telephone3;
+	}
+	public void setTelephone3(String telephone3) {
+		this.telephone3 = telephone3;
+	}
+	public String getMobile1() {
+		return mobile1;
+	}
+	public void setMobile1(String mobile1) {
+		this.mobile1 = mobile1;
+	}
+	public String getMobile2() {
+		return mobile2;
+	}
+	public void setMobile2(String mobile2) {
+		this.mobile2 = mobile2;
+	}
+	public String getMobile3() {
+		return mobile3;
+	}
+	public void setMobile3(String mobile3) {
+		this.mobile3 = mobile3;
+	}
+	public String getAddress1() {
+		return address1;
+	}
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+	public String getAddress2() {
+		return address2;
+	}
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+	public String getAddress3() {
+		return address3;
+	}
+	public void setAddress3(String address3) {
+		this.address3 = address3;
+	}
+	public String getDistrict() {
+		return district;
+	}
+	public void setDistrict(String district) {
+		this.district = district;
+	}	
+	
+	
+	
+}
