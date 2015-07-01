ea61c174e72e86a023daef1982787698f593529e
Jerry Wu <Jerry.JM.Wu@PCCW.COM>
2015-07-01 17:12:03 +0800

    add SaviePaymentBean

--- src/main/java/com/ifwd/fwdhk/model/savie/SaviePaymentBean.java
+++ src/main/java/com/ifwd/fwdhk/model/savie/SaviePaymentBean.java
@@ -12,6 +12,54 @@ public class SaviePaymentBean {
 	private String accountNo;
 	private Date expiryDate;
 	private boolean paymentConfirmAuthorize;
+	public boolean isPaymentType() {
+		return paymentType;
+	}
+	public void setPaymentType(boolean paymentType) {
+		this.paymentType = paymentType;
+	}
+	public float getAmount() {
+		return amount;
+	}
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+	public String getBankNameCode() {
+		return bankNameCode;
+	}
+	public void setBankNameCode(String bankNameCode) {
+		this.bankNameCode = bankNameCode;
+	}
+	public String getBranchName() {
+		return branchName;
+	}
+	public void setBranchName(String branchName) {
+		this.branchName = branchName;
+	}
+	public String getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+	public Date getExpiryDate() {
+		return expiryDate;
+	}
+	public void setExpiryDate(Date expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+	public boolean isPaymentConfirmAuthorize() {
+		return paymentConfirmAuthorize;
+	}
+	public void setPaymentConfirmAuthorize(boolean paymentConfirmAuthorize) {
+		this.paymentConfirmAuthorize = paymentConfirmAuthorize;
+	}
 	
 	
 	
