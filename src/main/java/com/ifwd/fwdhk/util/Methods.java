8b15632efa7aac6363af06bac60a95b9005dfc5f
Jerry Wu <Jerry.JM.Wu@PCCW.COM>
2015-07-02 11:07:13 +0800

    add file to base64 string

--- src/main/java/com/ifwd/fwdhk/util/Methods.java
+++ src/main/java/com/ifwd/fwdhk/util/Methods.java
@@ -1,6 +1,9 @@
 package com.ifwd.fwdhk.util;
 
 import java.util.regex.Pattern;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.lang.reflect.Field;
 import java.text.DecimalFormat;
 import java.text.ParseException;
@@ -200,4 +203,45 @@ public class Methods {
         
         return output;
      }
+    
+    /**
+     * <p>将文件转成base64 字符串</p>
+     * @param path 文件路径
+     * @return
+     * @throws Exception
+     */
+    public static String encodeBase64File(String path) throws Exception {
+        File  file = new File(path);
+        FileInputStream inputFile = new FileInputStream(file);
+        byte[] buffer = new byte[(int)file.length()];
+        inputFile.read(buffer);
+        inputFile.close();
+        return new sun.misc.BASE64Encoder().encode(buffer);
+    }
+    /**
+     * <p>将base64字符解码保存文件</p>
+     * @param base64Code
+     * @param targetPath
+     * @throws Exception
+     */
+    public static void decoderBase64File(String base64Code,String targetPath) throws Exception {
+        byte[] buffer = new sun.misc.BASE64Decoder().decodeBuffer(base64Code);
+        FileOutputStream out = new FileOutputStream(targetPath);
+        out.write(buffer);
+        out.close();
+    }
+    /**
+     * <p>将base64字符保存文本文件</p>
+     * @param base64Code
+     * @param targetPath
+     * @throws Exception
+     */ 
+    
+    public static void toFile(String base64Code,String targetPath) throws Exception {
+        byte[] buffer = base64Code.getBytes();
+        FileOutputStream out = new FileOutputStream(targetPath);
+        out.write(buffer);
+        out.close();
+    }
+    
 }
