e51567153301e9b75a71fd1312e1bb106b6a4f2f
Gavin <Gavin.B.Li@pccw.com>
2015-07-03 13:20:06 +0800

    modify

--- src/main/java/com/ifwd/fwdhk/util/InitApplicationMessage.java
+++ src/main/java/com/ifwd/fwdhk/util/InitApplicationMessage.java
@@ -7,15 +7,21 @@ import java.util.HashMap;
 
 import org.joda.time.LocalDate;
 import org.json.simple.JSONObject;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.ContextStartedEvent;
 import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
 
 import com.ifwd.fwdhk.api.controller.RestServiceDao;
 import com.ifwd.fwdhk.controller.UserRestURIConstants;
 import com.ifwd.fwdhk.model.QuoteDetails;
 
-public class InitApplicationMessage implements InitializingBean{
+@SuppressWarnings("rawtypes")
+@Component
+public class InitApplicationMessage implements ApplicationListener{
 	
 	public static QuoteDetails quoteDetails ;
 	
@@ -23,28 +29,31 @@ public class InitApplicationMessage implements InitializingBean{
 	private RestServiceDao restService;
 
 	@Override
-	public void afterPropertiesSet() throws Exception {
-		quoteDetails = new QuoteDetails();
-		LocalDate commencementDate = new LocalDate(new Date());
-		String Url = UserRestURIConstants.WORKINGHOLIDAY_GET_QUOTE + "?planCode=WorkingHoliday"
-				+ "&commencementDate=" + commencementDate + "&referralCode=" + "";
-
-		HashMap<String, String> header = new HashMap<String, String>(
-				COMMON_HEADERS);
-		
-		header.put("userName", "*DIRECTGI");
-		header.put("token", "a5684816-51b4-a2bc-fdd8-33887464726b");
-		
-		
-		
-		header.put("language", WebServiceUtils.transformLanaguage("CN"));
-		JSONObject responseJsonObj = restService.consumeApi(HttpMethod.GET,
-				Url, header, null);
-		
-		quoteDetails.setReferralCode((String)responseJsonObj.get("referralCode"));
-		quoteDetails.setReferralName((String)responseJsonObj.get("referralName"));
-		quoteDetails.setPlanCode((String)responseJsonObj.get("planCode"));
-		System.out.println("***********responseJsonObj****************:"+responseJsonObj);
-		
+	public void onApplicationEvent(ApplicationEvent event) {
+		if (event instanceof ContextStartedEvent || event instanceof ContextRefreshedEvent) {
+			quoteDetails = new QuoteDetails();
+			LocalDate commencementDate = new LocalDate(new Date());
+			String Url = UserRestURIConstants.WORKINGHOLIDAY_GET_QUOTE + "?planCode=WorkingHoliday"
+					+ "&commencementDate=" + commencementDate + "&referralCode=" + "";
+	
+			HashMap<String, String> header = new HashMap<String, String>(
+					COMMON_HEADERS);
+			
+			header.put("userName", "*DIRECTGI");
+			header.put("token", "a5684816-51b4-a2bc-fdd8-33887464726b");
+			
+			
+			
+			header.put("language", WebServiceUtils.transformLanaguage("CN"));
+			JSONObject responseJsonObj = restService.consumeApi(HttpMethod.GET,
+					Url, header, null);
+			
+			quoteDetails.setReferralCode((String)responseJsonObj.get("referralCode"));
+			quoteDetails.setReferralName((String)responseJsonObj.get("referralName"));
+			quoteDetails.setPlanCode((String)responseJsonObj.get("planCode"));
+			System.out.println("***********responseJsonObj****************:"+responseJsonObj);
+			
+		}
 	}
+	
 }
