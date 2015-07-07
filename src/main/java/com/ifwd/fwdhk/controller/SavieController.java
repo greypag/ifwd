c099abd693d005468d22d0766913ba5d356e1569
Peng_Xia <Peng_Xia@vandagroup.com>
2015-07-07 14:47:16 +0800

    update call getSavieIllustration

--- src/main/java/com/ifwd/fwdhk/controller/SavieController.java
+++ src/main/java/com/ifwd/fwdhk/controller/SavieController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ifwd.fwdhk.api.controller.RestServiceDao;
+import com.ifwd.fwdhk.model.QuoteDetails;
 import com.ifwd.fwdhk.model.savie.SavieFormApplicationBean;
 import com.ifwd.fwdhk.util.SaviePageFlowControl;
 import com.ifwd.fwdhk.util.WebServiceUtils;
@@ -82,68 +83,73 @@ public class SavieController {
 		HashMap<String, String> header = new HashMap<String, String>(
 				COMMON_HEADERS);
 		header.put("language", WebServiceUtils.transformLanaguage(lang));
+		logger.debug(Url.toString());
 		
 		org.json.simple.JSONObject apiJsonObj = restService.consumeApi(HttpMethod.GET,Url.toString(), header, null);
 		
-		String jsonStr = apiJsonObj.toJSONString();
-		logger.debug(jsonStr);
-		JSONObject responseJsonObj = JSONObject.fromObject(jsonStr);
-		logger.info("getSavieIllustrationByAjax API:" + responseJsonObj);
-		logger.info(responseJsonObj.getString("salesIllustration0Rate"));
-		
-		List<JSONObject> SalesIllustration0Rate = (List<JSONObject>) responseJsonObj.get("salesIllustration0Rate");
-		List<JSONObject> SalesIllustration2Rate = (List<JSONObject>) responseJsonObj.get("salesIllustration2Rate");
-		List<JSONObject> SalesIllustration3Rate = (List<JSONObject>) responseJsonObj.get("salesIllustration3Rate");
-		List<JSONObject> SalesIllustration4Rate = (List<JSONObject>) responseJsonObj.get("salesIllustration4Rate");
+		logger.info("apiJsonObj:"+apiJsonObj);
 		
 		JSONObject resultJsonObject = new JSONObject();
-		
-		List<JSONObject> inputTableList = new ArrayList<JSONObject>();
-		JSONObject inputTable = new JSONObject();
-		inputTable.accumulate("type", product);
-		inputTable.accumulate("issueAge", issueAge);
-		inputTable.accumulate("paymode", "monthly");
-		inputTable.accumulate("premium", premium);
-		inputTable.accumulate("paymentTerm", paymentTerm);
-		inputTable.accumulate("promoCode", referralCode);
-		inputTableList.add(inputTable);
-		
-		JSONObject salesIllustrationJsonObject = new JSONObject();
-		salesIllustrationJsonObject.accumulate("inputTable", inputTableList);
-		
-		List<JSONObject> yearPlansList = new ArrayList<JSONObject>();
-		
-		for(int i =0;i<SalesIllustration0Rate.size();i++){
-			JSONObject yesrPlan = new JSONObject();
-			yesrPlan.accumulate("year", Integer.valueOf(SalesIllustration0Rate.get(i).getString("type").substring(1)));
+		if(null == apiJsonObj.get("errMsgs")){
+			String jsonStr = apiJsonObj.toJSONString();
+			JSONObject responseJsonObj = JSONObject.fromObject(jsonStr);
+			logger.info("getSavieIllustrationByAjax API:" + responseJsonObj);
 			
-			List<JSONObject> plansList = new ArrayList<JSONObject>();
+			List<JSONObject> SalesIllustration0Rate = (List<JSONObject>) responseJsonObj.get("salesIllustration0Rate");
+			List<JSONObject> SalesIllustration2Rate = (List<JSONObject>) responseJsonObj.get("salesIllustration2Rate");
+			List<JSONObject> SalesIllustration3Rate = (List<JSONObject>) responseJsonObj.get("salesIllustration3Rate");
+			List<JSONObject> SalesIllustration4Rate = (List<JSONObject>) responseJsonObj.get("salesIllustration4Rate");
 			
-			JSONObject plan0 = new JSONObject();
-			plan0.accumulate("accountBalance", Float.valueOf(SalesIllustration0Rate.get(i).getString("accountEOP")));
-			plan0.accumulate("rate","zero");
-			plansList.add(plan0);
+			List<JSONObject> inputTableList = new ArrayList<JSONObject>();
+			JSONObject inputTable = new JSONObject();
+			inputTable.accumulate("type", product);
+			inputTable.accumulate("issueAge", issueAge);
+			inputTable.accumulate("paymode", "monthly");
+			inputTable.accumulate("premium", premium);
+			inputTable.accumulate("paymentTerm", paymentTerm);
+			inputTable.accumulate("promoCode", referralCode);
+			inputTableList.add(inputTable);
 			
-			JSONObject plan2 = new JSONObject();
-			plan2.accumulate("accountBalance", Float.valueOf(SalesIllustration2Rate.get(i).getString("accountEOP")));
-			plan2.accumulate("rate","two");
-			plansList.add(plan2);
+			JSONObject salesIllustrationJsonObject = new JSONObject();
+			salesIllustrationJsonObject.accumulate("inputTable", inputTableList);
 			
-			JSONObject plan3 = new JSONObject();
-			plan3.accumulate("accountBalance", Float.valueOf(SalesIllustration3Rate.get(i).getString("accountEOP")));
-			plan3.accumulate("rate","three");
-			plansList.add(plan3);
+			List<JSONObject> yearPlansList = new ArrayList<JSONObject>();
 			
-			JSONObject plan4 = new JSONObject();
-			plan4.accumulate("accountBalance", Float.valueOf(SalesIllustration4Rate.get(i).getString("accountEOP")));
-			plan4.accumulate("rate","four");
-			plansList.add(plan4);
-			
-			yesrPlan.accumulate("plans", plansList);
-			yearPlansList.add(yesrPlan);
+			for(int i =0;i<SalesIllustration0Rate.size();i++){
+				JSONObject yesrPlan = new JSONObject();
+				yesrPlan.accumulate("year", Integer.valueOf(SalesIllustration0Rate.get(i).getString("type").substring(1)));
+				
+				List<JSONObject> plansList = new ArrayList<JSONObject>();
+				
+				JSONObject plan0 = new JSONObject();
+				plan0.accumulate("accountBalance", Float.valueOf(SalesIllustration0Rate.get(i).getString("accountEOP")));
+				plan0.accumulate("rate","zero");
+				plansList.add(plan0);
+				
+				JSONObject plan2 = new JSONObject();
+				plan2.accumulate("accountBalance", Float.valueOf(SalesIllustration2Rate.get(i).getString("accountEOP")));
+				plan2.accumulate("rate","two");
+				plansList.add(plan2);
+				
+				JSONObject plan3 = new JSONObject();
+				plan3.accumulate("accountBalance", Float.valueOf(SalesIllustration3Rate.get(i).getString("accountEOP")));
+				plan3.accumulate("rate","three");
+				plansList.add(plan3);
+				
+				JSONObject plan4 = new JSONObject();
+				plan4.accumulate("accountBalance", Float.valueOf(SalesIllustration4Rate.get(i).getString("accountEOP")));
+				plan4.accumulate("rate","four");
+				plansList.add(plan4);
+				
+				yesrPlan.accumulate("plans", plansList);
+				yearPlansList.add(yesrPlan);
+			}
+	        salesIllustrationJsonObject.accumulate("yearPlans", yearPlansList);
+			resultJsonObject.accumulate("salesIllustration", salesIllustrationJsonObject);
 		}
-        salesIllustrationJsonObject.accumulate("yearPlans", yearPlansList);
-		resultJsonObject.accumulate("salesIllustration", salesIllustrationJsonObject);
+		else{
+			resultJsonObject.accumulate("salesIllustration", apiJsonObj.get("errMsgs"));
+		}
 		
 		logger.info(resultJsonObject.toString());
 		
@@ -177,7 +183,6 @@ public class SavieController {
 		String lang = UserRestURIConstants.getLanaguage(request);
 		if (lang.equals("tc"))
 			lang = "CN";
-		
 		logger.debug(savieDetail.getSavieApplicantBean().getFirstName());
 		logger.debug(savieDetail.getSavieEmploymentBean().getEmploymentStatus());
 		request.getSession().setAttribute("savieDetail", savieDetail);
