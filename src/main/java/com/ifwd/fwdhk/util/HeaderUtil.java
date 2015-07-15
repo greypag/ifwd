7212a191e30fc53826612e6c73cf3ad97e3ea42b
jon <xiangyan_chen@vandagroup.com>
2015-07-15 09:13:53 +0800

    HeaderUtil

---
+++ src/main/java/com/ifwd/fwdhk/util/HeaderUtil.java
@@ -0,0 +1,50 @@
+package com.ifwd.fwdhk.util;
+
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.google.common.collect.Maps;
+import com.ifwd.fwdhk.api.controller.RestServiceDao;
+import com.ifwd.fwdhk.controller.UserRestURIConstants;
+
+@Component
+public class HeaderUtil{
+	@Autowired
+	private RestServiceDao restService;
+	/**
+	 * init Header Map info
+	 * @param request HttpServletRequest request
+	 * @return Map<String,String>
+	 */
+	public Map<String,String> getHeader(HttpServletRequest request){
+		String token = null, username = null;
+		HttpSession session = request.getSession();
+		if((session.getAttribute("token") != null) && (session.getAttribute("username") != null)){
+			token = session.getAttribute("token").toString();
+			username = session.getAttribute("username").toString();
+		}else{
+			restService.consumeLoginApi(request);
+			if ((session.getAttribute("token") != null)) {
+				token = session.getAttribute("token").toString();
+				username = session.getAttribute("username").toString();
+			}
+		}
+		String lang = UserRestURIConstants.getLanaguage(request);
+		if (lang.equals("tc")){
+			lang = "CN";
+		}
+		Map<String,String> header = Maps.newHashMap();
+		//header.put("country", "HK");
+		header.put("language", lang);
+		header.put("token", token);
+		header.put("username", username);
+		return header;
+	}
+}
