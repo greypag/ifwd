526aa9a353d1624d324f8489e098c43c086a9c6a
Derek Chan <hk.derek.chan@gmail.com>
2015-11-02 18:31:54 +0800

    general approach for printing errors

--- src/main/java/com/ifwd/fwdhk/controller/TravelController.java
+++ src/main/java/com/ifwd/fwdhk/controller/TravelController.java
@@ -75,13 +75,13 @@ public class TravelController {
 			@RequestParam(required = false) final String utm_content)  {
 
 		//String promo = (String)request.getAttribute("promo");
-		String departureDate = (String)request.getParameter("departureDate");
-		String returnDate = (String)request.getParameter("returnDate");
-		String plan = (String)request.getParameter("plan");
-		String traveler = (String)request.getParameter("traveler");
-		String adult = (String)request.getParameter("adult");
-		String child = (String)request.getParameter("child");
-		String other = (String)request.getParameter("other");
+		String departureDate = request.getParameter("departureDate");
+		String returnDate = request.getParameter("returnDate");
+		String plan = request.getParameter("plan");
+		String traveler = request.getParameter("traveler");
+		String adult = request.getParameter("adult");
+		String child = request.getParameter("child");
+		String other = request.getParameter("other");
 		int iTraveler = 0;
 		int iAdult = 0;
 		int iChild = 0;
@@ -430,8 +430,9 @@ public class TravelController {
 			}
 			
 			String lang = UserRestURIConstants.getLanaguage(request);
-			if (lang.equals("tc"))
+			if (lang.equals("tc")) {
 				lang = "CN";
+			}
 			
 			header.put("language", WebServiceUtils.transformLanaguage(lang));
 			JSONObject responseJsonObj = restService.consumeApi(HttpMethod.GET,
@@ -573,8 +574,9 @@ public class TravelController {
 					+ expiryDate + "&referralCode=" + promoCode;
 
 			String lang = UserRestURIConstants.getLanaguage(request);
-			if (lang.equals("tc"))
+			if (lang.equals("tc")) {
 				lang = "CN";
+			}
 			
 			HashMap<String, String> header = new HashMap<String, String>(
 					COMMON_HEADERS);
@@ -765,8 +767,9 @@ public class TravelController {
 					COMMON_HEADERS);
 			
 			String lang = UserRestURIConstants.getLanaguage(request);
-			if (lang.equals("tc"))
+			if (lang.equals("tc")) {
 				lang = "CN";
+			}
 			
 			header.put("language", WebServiceUtils
 					.transformLanaguage(lang));
@@ -1885,7 +1888,8 @@ public class TravelController {
 					parameters);
 			logger.info("TRAVEL_FINALIZE_POLICY Response " + responsObject);
 			
-			if (responsObject.get("errMsgs") == null) {
+			Object errMsgs = responsObject.get("errMsgs");
+			if (errMsgs == null) {
 				session.removeAttribute("creditCardNo");
 				session.removeAttribute("expiryDate");
 				session.removeAttribute("upgradeTotalTravallingDays");
@@ -1913,19 +1917,21 @@ public class TravelController {
 				return UserRestURIConstants.getSitePath(request)
 						+ "travel/travel-confirmation";
 			} else {
-				if (responsObject.get("errMsgs").toString().contains("invalid payment amount")) {
+				// FIXME this is duplicated in HomeCareController & others, better move up this logic to parent class or somewhere else.
+				if (errMsgs.toString().contains("invalid payment amount")) {
 					model.addAttribute("errorHeader1", "Invalid Payment Amount");
 					model.addAttribute("errorDescription1", "There is a mismatch of the payment amount with the policy");
 					model.addAttribute("errorHeader2", "Please DO NOT retry the payment");
 					model.addAttribute("errorDescription2", "Contact our CS at 3123 3123");
+				} else if(errMsgs instanceof JSONArray){
+					JSONArray errors = (JSONArray)errMsgs;
+					model.addAttribute("errMsgs", errors.toArray());					
 				} else {
 					model.addAttribute("errorHeader1", UserRestURIConstants.ERROR_HEADER1_1 + email + UserRestURIConstants.ERROR_HEADER1_2);
 					model.addAttribute("errorDescription1", UserRestURIConstants.ERROR_DESCRIPTION1 + " " + requestNo);
 					model.addAttribute("errorHeader2", UserRestURIConstants.ERROR_HEADER2_1 + " " + email + UserRestURIConstants.ERROR_HEADER2_2);
 					model.addAttribute("errorDescription2", UserRestURIConstants.ERROR_DESCRIPTION2 + " " + requestNo);
 				}		
-
-				
 				
 				return UserRestURIConstants.getSitePath(request)
 						+ "error";
@@ -2013,14 +2019,15 @@ public class TravelController {
 		header.put("token", tokenInSession);
 		header.put("language", WebServiceUtils
 				.transformLanaguage(UserRestURIConstants.getLanaguage(request)));
-		if (planCode == null)
+		if (planCode == null) {
 			planCode = "TRAVELCARE";
-		if (planCode.toUpperCase().equals("HOMECARE"))
-		
-		// String referalCOde = session.getAttribute("referralCode").toString();
+		}
+		if (planCode.toUpperCase().equals("HOMECARE")) {
+			// String referalCOde = session.getAttribute("referralCode").toString();
 			mailed = sendEmail.sendEmail(emailToSendPromoCode, "ECHOME", header);
-		else
+		} else {
 			mailed = sendEmail.sendEmail(emailToSendPromoCode, "TRA123", header);
+		}
 		if (mailed) {
 			return "success";
 		} else {
@@ -2143,8 +2150,9 @@ public class TravelController {
 			}
 			
 			String lang = UserRestURIConstants.getLanaguage(request);
-			if (lang.equals("tc"))
+			if (lang.equals("tc")) {
 				lang = "CN";
+			}
 			
 			header.put("language", WebServiceUtils.transformLanaguage(lang));
 			JSONObject responseJsonObj = restService.consumeApi(HttpMethod.GET,
