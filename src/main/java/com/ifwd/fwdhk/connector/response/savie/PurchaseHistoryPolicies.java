cf48e578be2b00421161367285eb54b6c0a3624d
Kris <Peng_Xia@vandagroup.com>
2015-11-06 11:38:13 +0800

    Fix elite term useraccount

--- src/main/java/com/ifwd/fwdhk/connector/response/savie/PurchaseHistoryPolicies.java
+++ src/main/java/com/ifwd/fwdhk/connector/response/savie/PurchaseHistoryPolicies.java
@@ -5,8 +5,16 @@ public class PurchaseHistoryPolicies{
 	private String submissionDate;
 	private String policyNumber;
 	private String amount;
+	private String totalDue;
+	private String discount;
 	private String commencementDate;
 	private String expiryDate;
+	private String referralCode;
+	private String promoCode;
+	private String promotionType;
+	private String status;
+	private String policyType;
+	
 	public String getPlanCode() {
 		return planCode;
 	}
@@ -42,6 +50,48 @@ public class PurchaseHistoryPolicies{
 	}
 	public void setExpiryDate(String expiryDate) {
 		this.expiryDate = expiryDate;
+	}
+	public String getTotalDue() {
+		return totalDue;
+	}
+	public void setTotalDue(String totalDue) {
+		this.totalDue = totalDue;
+	}
+	public String getDiscount() {
+		return discount;
+	}
+	public void setDiscount(String discount) {
+		this.discount = discount;
+	}
+	public String getReferralCode() {
+		return referralCode;
+	}
+	public void setReferralCode(String referralCode) {
+		this.referralCode = referralCode;
+	}
+	public String getPromoCode() {
+		return promoCode;
+	}
+	public void setPromoCode(String promoCode) {
+		this.promoCode = promoCode;
+	}
+	public String getPromotionType() {
+		return promotionType;
+	}
+	public void setPromotionType(String promotionType) {
+		this.promotionType = promotionType;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getPolicyType() {
+		return policyType;
+	}
+	public void setPolicyType(String policyType) {
+		this.policyType = policyType;
 	}
 	
 	
