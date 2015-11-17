d8c98573e17040978d221fc3ddc8b5c241507179
Kris <Peng_Xia@vandagroup.com>
2015-11-17 11:13:17 +0800

    Add elite term occupation session when start server

--- src/main/java/com/ifwd/fwdhk/util/InitApplicationMessage.java
+++ src/main/java/com/ifwd/fwdhk/util/InitApplicationMessage.java
@@ -79,6 +79,55 @@ public class InitApplicationMessage implements ApplicationListener{
 	public static List<OptionItemDesc> etEducationLevelEN;
 	public static List<OptionItemDesc> etEducationLevelCN;
 	
+	public static List<OptionItemDesc> occupationEnNoB1;
+	public static List<OptionItemDesc> occupationCnNoB1;
+	public static List<OptionItemDesc> occupationEnNoB2;
+	public static List<OptionItemDesc> occupationCnNoB2;
+	public static List<OptionItemDesc> occupationEnNoB3;
+	public static List<OptionItemDesc> occupationCnNoB3;
+	public static List<OptionItemDesc> occupationEnNoB4;
+	public static List<OptionItemDesc> occupationCnNoB4;
+	public static List<OptionItemDesc> occupationEnNoB5;
+	public static List<OptionItemDesc> occupationCnNoB5;
+	public static List<OptionItemDesc> occupationEnNoB6;
+	public static List<OptionItemDesc> occupationCnNoB6;
+	public static List<OptionItemDesc> occupationEnNoB7;
+	public static List<OptionItemDesc> occupationCnNoB7;
+	public static List<OptionItemDesc> occupationEnNoB8;
+	public static List<OptionItemDesc> occupationCnNoB8;
+	public static List<OptionItemDesc> occupationEnNoB9;
+	public static List<OptionItemDesc> occupationCnNoB9;
+	public static List<OptionItemDesc> occupationEnNoB10;
+	public static List<OptionItemDesc> occupationCnNoB10;
+	public static List<OptionItemDesc> occupationEnNoB11;
+	public static List<OptionItemDesc> occupationCnNoB11;
+	public static List<OptionItemDesc> occupationEnNoB12;
+	public static List<OptionItemDesc> occupationCnNoB12;
+	public static List<OptionItemDesc> occupationEnNoB13;
+	public static List<OptionItemDesc> occupationCnNoB13;
+	public static List<OptionItemDesc> occupationEnNoB14;
+	public static List<OptionItemDesc> occupationCnNoB14;
+	public static List<OptionItemDesc> occupationEnNoB15;
+	public static List<OptionItemDesc> occupationCnNoB15;
+	public static List<OptionItemDesc> occupationEnNoB16;
+	public static List<OptionItemDesc> occupationCnNoB16;
+	public static List<OptionItemDesc> occupationEnNoB17;
+	public static List<OptionItemDesc> occupationCnNoB17;
+	public static List<OptionItemDesc> occupationEnNoB18;
+	public static List<OptionItemDesc> occupationCnNoB18;
+	public static List<OptionItemDesc> occupationEnNoB19;
+	public static List<OptionItemDesc> occupationCnNoB19;
+	public static List<OptionItemDesc> occupationEnNoB20;
+	public static List<OptionItemDesc> occupationCnNoB20;
+	public static List<OptionItemDesc> occupationEnNoB21;
+	public static List<OptionItemDesc> occupationCnNoB21;
+	public static List<OptionItemDesc> occupationEnNoB22;
+	public static List<OptionItemDesc> occupationCnNoB22;
+	public static List<OptionItemDesc> occupationEnNoB23;
+	public static List<OptionItemDesc> occupationCnNoB23;
+	public static List<OptionItemDesc> occupationEnNoB24;
+	public static List<OptionItemDesc> occupationCnNoB24;
+	
 
 	@Override
 	public void onApplicationEvent(ApplicationEvent event) {
@@ -443,6 +492,485 @@ public class InitApplicationMessage implements ApplicationListener{
 			}
 		}
 		logger.info("etEducationLevelCN : " + etEducationLevelCN);
+		
+		try {
+			occupationEnNoB1 = commonUtils.getOptionItemDescList("NoB1","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB1 : " + occupationEnNoB1);
+		
+		try {
+			occupationCnNoB1 = commonUtils.getOptionItemDescList("NoB1","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB1 : " + occupationCnNoB1);
+		
+		try {
+			occupationEnNoB2 = commonUtils.getOptionItemDescList("NoB2","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB2 : " + occupationEnNoB2);
+		
+		try {
+			occupationCnNoB2 = commonUtils.getOptionItemDescList("NoB2","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB2 : " + occupationCnNoB2);
+		
+		try {
+			occupationEnNoB3 = commonUtils.getOptionItemDescList("NoB3","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB3 : " + occupationEnNoB3);
+		
+		try {
+			occupationCnNoB3 = commonUtils.getOptionItemDescList("NoB3","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB3 : " + occupationCnNoB3);
+		
+		try {
+			occupationEnNoB4 = commonUtils.getOptionItemDescList("NoB4","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB4 : " + occupationEnNoB4);
+		
+		try {
+			occupationCnNoB4 = commonUtils.getOptionItemDescList("NoB4","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB4 : " + occupationCnNoB4);
+		
+		try {
+			occupationEnNoB5 = commonUtils.getOptionItemDescList("NoB5","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB5 : " + occupationEnNoB5);
+		
+		try {
+			occupationCnNoB5 = commonUtils.getOptionItemDescList("NoB5","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB5 : " + occupationCnNoB5);
+		
+		try {
+			occupationEnNoB6 = commonUtils.getOptionItemDescList("NoB6","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB6 : " + occupationEnNoB6);
+		
+		try {
+			occupationCnNoB6 = commonUtils.getOptionItemDescList("NoB6","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB6 : " + occupationCnNoB6);
+		
+		try {
+			occupationEnNoB7 = commonUtils.getOptionItemDescList("NoB7","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB7 : " + occupationEnNoB7);
+		
+		try {
+			occupationCnNoB7 = commonUtils.getOptionItemDescList("NoB7","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB7 : " + occupationCnNoB7);
+		
+		try {
+			occupationEnNoB8 = commonUtils.getOptionItemDescList("NoB8","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB8 : " + occupationEnNoB8);
+		
+		try {
+			occupationCnNoB8 = commonUtils.getOptionItemDescList("NoB8","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB8 : " + occupationCnNoB8);
+		
+		try {
+			occupationEnNoB9 = commonUtils.getOptionItemDescList("NoB9","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB9 : " + occupationEnNoB9);
+		
+		try {
+			occupationCnNoB9 = commonUtils.getOptionItemDescList("NoB9","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB9 : " + occupationCnNoB9);
+		
+		try {
+			occupationEnNoB10 = commonUtils.getOptionItemDescList("NoB10","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB10 : " + occupationEnNoB10);
+		
+		try {
+			occupationCnNoB10 = commonUtils.getOptionItemDescList("NoB10","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB10 : " + occupationCnNoB10);
+		
+		try {
+			occupationEnNoB11 = commonUtils.getOptionItemDescList("NoB11","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB11 : " + occupationEnNoB11);
+		
+		try {
+			occupationCnNoB11 = commonUtils.getOptionItemDescList("NoB11","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB11 : " + occupationCnNoB11);
+		
+		try {
+			occupationEnNoB12 = commonUtils.getOptionItemDescList("NoB12","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB12 : " + occupationEnNoB12);
+		
+		try {
+			occupationCnNoB12 = commonUtils.getOptionItemDescList("NoB12","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB12 : " + occupationCnNoB12);
+		
+		try {
+			occupationEnNoB13 = commonUtils.getOptionItemDescList("NoB13","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB13 : " + occupationEnNoB13);
+		
+		try {
+			occupationCnNoB13 = commonUtils.getOptionItemDescList("NoB13","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB13 : " + occupationCnNoB13);
+		
+		try {
+			occupationEnNoB14 = commonUtils.getOptionItemDescList("NoB14","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB14 : " + occupationEnNoB14);
+		
+		try {
+			occupationCnNoB14 = commonUtils.getOptionItemDescList("NoB14","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB14 : " + occupationCnNoB14);
+		
+		try {
+			occupationEnNoB15 = commonUtils.getOptionItemDescList("NoB15","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB15 : " + occupationEnNoB15);
+		
+		try {
+			occupationCnNoB15 = commonUtils.getOptionItemDescList("NoB15","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB15 : " + occupationCnNoB15);
+		
+		try {
+			occupationEnNoB16 = commonUtils.getOptionItemDescList("NoB16","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB16 : " + occupationEnNoB16);
+		
+		try {
+			occupationCnNoB16 = commonUtils.getOptionItemDescList("NoB16","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB16 : " + occupationCnNoB16);
+		
+		try {
+			occupationEnNoB17 = commonUtils.getOptionItemDescList("NoB17","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB17 : " + occupationEnNoB17);
+		
+		try {
+			occupationCnNoB17 = commonUtils.getOptionItemDescList("NoB17","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB17 : " + occupationCnNoB17);
+		
+		try {
+			occupationEnNoB18 = commonUtils.getOptionItemDescList("NoB18","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB18 : " + occupationEnNoB18);
+		
+		try {
+			occupationCnNoB18 = commonUtils.getOptionItemDescList("NoB18","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB18 : " + occupationCnNoB18);
+		
+		try {
+			occupationEnNoB19 = commonUtils.getOptionItemDescList("NoB19","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB19 : " + occupationEnNoB19);
+		
+		try {
+			occupationCnNoB19 = commonUtils.getOptionItemDescList("NoB19","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB19 : " + occupationCnNoB19);
+		
+		try {
+			occupationEnNoB20 = commonUtils.getOptionItemDescList("NoB20","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB20 : " + occupationEnNoB20);
+		
+		try {
+			occupationCnNoB20 = commonUtils.getOptionItemDescList("NoB20","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB20 : " + occupationCnNoB20);
+		
+		try {
+			occupationEnNoB21 = commonUtils.getOptionItemDescList("NoB21","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB21 : " + occupationEnNoB21);
+		
+		try {
+			occupationCnNoB21 = commonUtils.getOptionItemDescList("NoB21","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB21 : " + occupationCnNoB21);
+		
+		try {
+			occupationEnNoB22 = commonUtils.getOptionItemDescList("NoB22","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB22 : " + occupationEnNoB22);
+		
+		try {
+			occupationCnNoB22 = commonUtils.getOptionItemDescList("NoB22","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB22 : " + occupationCnNoB22);
+		
+		try {
+			occupationEnNoB23 = commonUtils.getOptionItemDescList("NoB23","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB23 : " + occupationEnNoB23);
+		
+		try {
+			occupationCnNoB23 = commonUtils.getOptionItemDescList("NoB23","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB23 : " + occupationCnNoB23);
+		
+		try {
+			occupationEnNoB24 = commonUtils.getOptionItemDescList("NoB24","EN",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationEnNoB24 : " + occupationEnNoB24);
+		
+		try {
+			occupationCnNoB24 = commonUtils.getOptionItemDescList("NoB24","CH",type);
+		} catch (Exception e) {
+			logger.error("error : "+e.getMessage());
+			if("start".equals(type)){
+				System.exit(0);
+			}
+		}
+		logger.info("occupationCnNoB24 : " + occupationCnNoB24);
 	}
-	
 }
