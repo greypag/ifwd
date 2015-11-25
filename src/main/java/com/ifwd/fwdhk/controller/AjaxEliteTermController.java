2d7e91ed57fa74ef44479cb8dd95b7935ac5966b
jon <xiangyan_chen@vandagroup.com>
2015-11-25 11:38:18 +0800

    delete watermark to upload img code

--- src/main/java/com/ifwd/fwdhk/controller/AjaxEliteTermController.java
+++ src/main/java/com/ifwd/fwdhk/controller/AjaxEliteTermController.java
@@ -73,9 +73,6 @@ public class AjaxEliteTermController extends BaseController{
 		        File toFile = new File(uploadDir + sep  
 				                + realName);
 		        ImgUtil.ImageToPdfToJPG(uploadDir + sep+ fileName, uploadDir + sep + name + ".pdf", toFile , request);
-		        String copyImagePath = request.getRealPath("/")+"resources/images/elite-terms/COPY.png";
-		        File copy = new File(copyImagePath);
-		        ImgUtil.pressImage(copy, toFile, 0, 0);
 //				ImgUtil.changeImageToJPG(uploadedFile,toFile,request);
 		        response.getWriter().write("true");
 			} catch (ECOMMAPIException e) {
