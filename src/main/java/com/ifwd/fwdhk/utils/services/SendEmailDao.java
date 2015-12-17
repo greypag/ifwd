6351c5e5452d311688729de09ecb36063084df26
Andy <Site_Wang@vandagroup.com>
2015-12-17 17:19:26 +0800

    send fwdiscover email

--- src/main/java/com/ifwd/fwdhk/utils/services/SendEmailDao.java
+++ src/main/java/com/ifwd/fwdhk/utils/services/SendEmailDao.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Component;
 @Component
 public interface SendEmailDao {
 	public boolean sendEmail(String emailId, String promotionalCode, HashMap<String, String> header);
-	public boolean sendEmailByDiscover(String username, String planName, String emailId, String discover, HashMap<String, String> header);
+	public boolean sendEmailByDiscover(HttpServletRequest request, String username,
+			String discount, String planName, String code, String date, String emailId,
+			HashMap<String, String> header);
 	public boolean sendY5buddyEmail(HttpServletRequest request, String recipientEmail, HashMap<String, String> header);
 	public boolean sendPromotionEmail(String emailId, HashMap<String, String> header);
 }
