87620d4208d598a4c47f340a3af2ffc4f95814b6
Kris <Peng_Xia@vandagroup.com>
2016-02-23 11:52:34 +0800

    Fix life-employment-info save for later

--- src/main/java/com/ifwd/fwdhk/controller/UserRestURIConstants.java
+++ src/main/java/com/ifwd/fwdhk/controller/UserRestURIConstants.java
@@ -149,7 +149,7 @@ public class UserRestURIConstants {
 	public static final String CREATE_LIFE_POLICY = "/life/policies";
 	public static final String FINALIZE_LIFE_POLICY = "/life/policies";
 	
-	public static final String CREATE_POLICY_APPLICATION_SAVE_FOR_LATER = SERVICE_URL + "/policies/application/saveForLater";
+	public static final String CREATE_POLICY_APPLICATION_SAVE_FOR_LATER = "/policies/application/saveForLater";
 	public static final String GET_POLICY_APPLICATION_SAVE_FOR_LATER = "/policies/application/saveForLater?planCode=SAVIE-SP";
 	/* Page Flow Control For Savie */
 	
