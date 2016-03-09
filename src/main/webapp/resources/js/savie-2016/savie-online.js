b348b3ea5a1d08f5fd341d36a6712c5f0f5f2516
Dennis Abasa <dennis.abasa@smartdrop.com.hk>
2016-03-09 20:18:20 +0800

    - #251 Merge the homepage.jsp that we developed to their current homepage

--- src/main/webapp/resources/js/savie-2016/savie-online.js
+++ src/main/webapp/resources/js/savie-2016/savie-online.js
@@ -1,57 +1,5 @@
 // Savie Online
-$(document).ready(function() {
-	window.onresize = function() {
-		if (getWidth()>=992) {
-			$('.psa-collapse-content').addClass('in');
-			$('.psa-btn').removeAttr('data-toggle');
-			
-			$('.fna-tab-content').addClass('col-lg-3 col-md-3 active');
-			$('.navtabs-title').removeAttr('data-toggle');
-			
-			// Carousel modification
-			$('.fna-icons-body .tab-content').removeClass('carousel-inner');
-			$('.fna-icons-body .tab-content').removeAttr('role');
-			$('.fna-tab-content').removeClass('item');
-			
-		} else {
-			$('.psa-collapse-content').removeClass('in');
-			$('.psa-btn').attr('data-toggle', 'collapse');
-			
-			$('.fna-tab-content').removeClass('col-lg-3 col-md-3 active');
-			$('.navtabs-title').attr('data-toggle', 'tab');
-			$('#navtabs-1').addClass('active');
-			
-			// Carousel modification
-			$('#so-carousel').carousel({
-				interval: false
-			});
-			$('#so-carousel').addClass('carousel slide');
-			$('#so-carousel').attr('data-ride', 'carousel');
-			$('.fna-carousel .tab-content').addClass('carousel-inner');
-			$('.fna-carousel .tab-content').attr('role', 'listbox');
-			$('.fna-tab-content').addClass('item');
-			
-			$('#so-carousel').on('slid.bs.carousel', function() {
-				if($('#navtabs-1').hasClass('active')) {
-					$('.navtabs-li').removeClass('active');
-					$('#nav-title-1').addClass('active');
-				}
-				if($('#navtabs-2').hasClass('active')) {
-					$('.navtabs-li').removeClass('active');
-					$('#nav-title-2').addClass('active');
-				}
-				if($('#navtabs-3').hasClass('active')) {
-					$('.navtabs-li').removeClass('active');
-					$('#nav-title-3').addClass('active');
-				}
-				if($('#navtabs-4').hasClass('active')) {
-					$('.navtabs-li').removeClass('active');
-					$('#nav-title-4').addClass('active');
-				}
-			});
-		}
-	};
-	$(window).resize();
+$(document).ready(function() {	
 	
 	// PDF lightbox resizing
 	var browWidth = $(window).width();
