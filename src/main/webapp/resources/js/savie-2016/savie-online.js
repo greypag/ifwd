25cc9f0624fff243b0cd6409c8b24fc8a6c9e57e
unknown <cmuser@5CG5072B5W.hk.intranet>
2016-04-06 11:01:58 +0800

    ticket 1685 fix

--- src/main/webapp/resources/js/savie-2016/savie-online.js
+++ src/main/webapp/resources/js/savie-2016/savie-online.js
@@ -36,28 +36,30 @@ $(document).ready(function() {
 	 });*/
 	 
 	// Placeholder	 
-	$('input[placeholder]').placeholder();
-	$('[placeholder]').focus(function() {
-		var input = $(this);
-		if (input.val() == input.attr('placeholder')) {
-			input.val('');
-			input.removeClass('placeholder');
-		}
-	}).blur(function() {
-		var input = $(this);
-		if (input.val() == '' || input.val() == input.attr('placeholder')) {
-			input.addClass('placeholder');
-			input.val(input.attr('placeholder'));
-		}
-	}).blur();
-	$('[placeholder]').parents('form').submit(function() {
-		$(this).find('[placeholder]').each(function() {
+    $('input[placeholder]').placeholder();
+    if(msieversion == 9){
+		$('[placeholder]').focus(function() {
 			var input = $(this);
 			if (input.val() == input.attr('placeholder')) {
 				input.val('');
+				input.removeClass('placeholder');
 			}
-		})
-	});
+		}).blur(function() {
+			var input = $(this);
+			if (input.val() == '' || input.val() == input.attr('placeholder')) {
+				input.addClass('placeholder');
+				input.val(input.attr('placeholder'));
+			}
+		}).blur();
+		$('[placeholder]').parents('form').submit(function() {
+			$(this).find('[placeholder]').each(function() {
+				var input = $(this);
+				if (input.val() == input.attr('placeholder')) {
+					input.val('');
+				}
+			})
+		});
+    }
 	
 	// Permanent Address showing the additional 3 lines
 	$('.res-textfield-hidden').css("margin-bottom", "0");
