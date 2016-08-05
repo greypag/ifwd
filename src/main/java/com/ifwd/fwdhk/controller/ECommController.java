0684f732cd7394bea7c9de79fbbb83ea195fbdfb
John Huang <John.MX.Huang@pccw.com>
2016-08-05 18:33:41 +0800

    change || to &&

--- src/main/java/com/ifwd/fwdhk/controller/ECommController.java
+++ src/main/java/com/ifwd/fwdhk/controller/ECommController.java
@@ -52,10 +52,8 @@ public class ECommController extends BaseController {
 	public ModelAndView changeLang(HttpServletRequest request,
 			@RequestParam String selectLang, @RequestParam String action, HttpServletResponse response) throws IOException {
 		
-		if (action.contains("utm_source")||action.contains("utm_medium")||action.contains("utm_campaign")){
+		if (action.contains("utm_source") && action.contains("utm_medium") && action.contains("utm_campaign")){
 			action=action.substring(0,action.indexOf("?"));
-			
-			
 		}
 		if (!action.toLowerCase().contains("/tc/") && !action.toLowerCase().contains("/en/") && !action.contains("joinus") && !action.contains("/error/")) {
 			response.sendError(HttpServletResponse.SC_BAD_REQUEST);			
