3c6f115b330c29c5be022b999d84760535fd5fee
Stephen <stephen.cf.lai@fwd.com>
2016-08-21 23:33:25 +0800

    added new classes and update code for swagger implementation

--- src/main/java/com/ifwd/fwdhk/controller/BaseController.java
+++ src/main/java/com/ifwd/fwdhk/controller/BaseController.java
@@ -1,14 +1,46 @@
 package com.ifwd.fwdhk.controller;
 
-
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ifwd.fwdhk.api.controller.RestServiceDao;
+import com.ifwd.fwdhk.connector.ECommWsConnector;
+import com.ifwd.fwdhk.util.Methods;
 
 
-public abstract class BaseController{
-
+public abstract class BaseController {
+	
+	@Autowired
+	protected RestServiceDao restService;
+	
+	@Autowired 
+	protected ECommWsConnector connector;
+	
+	protected boolean IsAuthenticate(HttpServletRequest request) {
+		// TODO, check config and allow external request
+		if (Methods.isXssAjax(request)) {
+			throw new RuntimeException("Unauthentication");
+		} else {
+			return true;
+		}
+	}
+		
+	protected String checkJsonObjNull(JSONObject obj, String checkByStr) {
+		if (obj.get(checkByStr) != null) {
+			return obj.get(checkByStr).toString().trim();
+		} else {
+			return "";
+		}
+	}	
+	
+	protected String washGIMessage(String originalErrMsg) {
+		return originalErrMsg.replaceAll("\"", "").replace("[", "").replace("]", "");
+	}
+	
 	protected void ajaxReturn(HttpServletResponse response, Object object){
 		response.setContentType("text/json;charset=utf-8");
 		//return data
