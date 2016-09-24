bc27a1e6c0af03605efc02abae9e78d057277266
Stephen <stephen.cf.lai@fwd.com>
2016-09-24 08:47:35 +0800

    updated email subject, vp#3726

--- src/main/java/com/ifwd/fwdhk/services/impl/LifeServiceImpl.java
+++ src/main/java/com/ifwd/fwdhk/services/impl/LifeServiceImpl.java
@@ -3551,10 +3551,10 @@ public class LifeServiceImpl implements LifeService {
 			subject = "FWD Medical Insurance Plan – Document Upload [" + lifePolicy.getPolicyNo() + "] | 富衛醫療保險 – 上載檔案 [" + lifePolicy.getPolicyNo() + "]";
 			template = "rophi\\uploadDocument.html";
 		}else if("provie-o2o-rp".equals(action)) {
-			subject = "Provie Appointment Acknowledgement from FWD | Provie真息揀理財壽險計劃申請確認";
+			subject = "Appointment Acknowledgement from FWD 富衛預約申請確認";
 			template = "provie\\provie-o2o-rp.html";
 		}else if("provie-o2o-sp".equals(action)) {
-			subject = "Provie Appointment Acknowledgement from FWD | Provie真息揀理財壽險計劃申請確認";
+			subject = "Appointment Acknowledgement from FWD 富衛預約申請確認";
 			template = "provie\\provie-o2o-sp.html";
 		}
 		
