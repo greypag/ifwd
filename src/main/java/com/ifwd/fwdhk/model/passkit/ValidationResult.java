2867145df70ba343c69fe29474bd9afbba30c11a
John Huang <John.MX.Huang@pccw.com>
2016-10-17 16:37:56 +0800

    bug fix

---
+++ src/main/java/com/ifwd/fwdhk/model/passkit/ValidationResult.java
@@ -0,0 +1,18 @@
+package com.ifwd.fwdhk.model.passkit;
+
+
+public class ValidationResult {
+	
+	private boolean valid;
+
+	public boolean isValid() {
+		return valid;
+	}
+
+	public void setValid(boolean valid) {
+		this.valid = valid;
+	}
+	
+
+
+}
