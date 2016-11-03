456173d4798b4a08091a50427495cd55b65a6526
Jerry Wu <Jerry.JM.Wu@pccw.com>
2016-11-03 18:07:22 +0800

    remove method = RequestMethod.POST

--- src/main/java/com/ifwd/fwdhk/controller/MotorController.java
+++ src/main/java/com/ifwd/fwdhk/controller/MotorController.java
@@ -26,67 +26,67 @@ public class MotorController extends BaseController{
 		return MotorPageFlowControl.pageFlow("",model,request, UserRestURIConstants.PAGE_PROPERTIES_MOTOR_GET_QUOTE);
 	}
 	
-	@RequestMapping(value = {"/{lang}/motor-insurance/third-party-quote"}, method = RequestMethod.POST)
+	@RequestMapping(value = {"/{lang}/motor-insurance/third-party-quote"})
 	public ModelAndView getPlanThird(Model model, HttpServletRequest request) {
 		return MotorPageFlowControl.pageFlow("", model, request, UserRestURIConstants.PAGE_PROPERTIES_MOTOR_PLAN_THIRD);
 	}
 	
-	@RequestMapping(value = {"/{lang}/motor-insurance/comprehensive-quote"}, method = RequestMethod.POST)
+	@RequestMapping(value = {"/{lang}/motor-insurance/comprehensive-quote"})
 	public ModelAndView getPlanComp(Model model, HttpServletRequest request) {
 		return MotorPageFlowControl.pageFlow("", model, request, UserRestURIConstants.PAGE_PROPERTIES_MOTOR_PLAN_COMP);
 	}
 	
-	@RequestMapping(value = {"/{lang}/motor-insurance/rider-options"}, method = RequestMethod.POST)
+	@RequestMapping(value = {"/{lang}/motor-insurance/rider-options"})
 	public ModelAndView getQuickQuote(Model model, HttpServletRequest request) {
 		return MotorPageFlowControl.pageFlow("", model, request, UserRestURIConstants.PAGE_PROPERTIES_MOTOR_QUICK_QUOTE);
 	}
 	
-	@RequestMapping(value = {"/{lang}/motor-insurance/car-details"}, method = RequestMethod.POST)
+	@RequestMapping(value = {"/{lang}/motor-insurance/car-details"})
 	public ModelAndView cardetails(Model model, HttpServletRequest request) {
 		return MotorPageFlowControl.pageFlow("", model, request, UserRestURIConstants.PAGE_PROPERTIES_MOTOR_CAR_DETAILS);
 	}
 	
-	@RequestMapping(value = {"/{lang}/motor-insurance/drivers-details"}, method = RequestMethod.POST)
+	@RequestMapping(value = {"/{lang}/motor-insurance/drivers-details"})
 	public ModelAndView driversdetails(Model model, HttpServletRequest request) {
 		return MotorPageFlowControl.pageFlow("", model, request, UserRestURIConstants.PAGE_PROPERTIES_MOTOR_DRIVERS_DETAILS);
 	}
 
-	@RequestMapping(value = {"/{lang}/motor-insurance/policy-details"}, method = RequestMethod.POST)
+	@RequestMapping(value = {"/{lang}/motor-insurance/policy-details"})
 	public ModelAndView policydetails(Model model, HttpServletRequest request) {
 		return MotorPageFlowControl.pageFlow("", model, request, UserRestURIConstants.PAGE_PROPERTIES_MOTOR_POLICY_DETAILS);
 	}
 	
-	@RequestMapping(value = {"/{lang}/motor-insurance/declarations"}, method = RequestMethod.POST)
+	@RequestMapping(value = {"/{lang}/motor-insurance/declarations"})
 	public ModelAndView declarations(Model model, HttpServletRequest request) {
 		return MotorPageFlowControl.pageFlow("", model, request, UserRestURIConstants.PAGE_PROPERTIES_MOTOR_DECLARATIONS);
 	}
 	
-	@RequestMapping(value = {"/{lang}/motor-insurance/application-summary"}, method = RequestMethod.POST)
+	@RequestMapping(value = {"/{lang}/motor-insurance/application-summary"})
 	public ModelAndView applicationsummary(Model model, HttpServletRequest request) {
 		return MotorPageFlowControl.pageFlow("", model, request, UserRestURIConstants.PAGE_PROPERTIES_MOTOR_APPLICATION_SUMMARY);
 	}
 	
-	@RequestMapping(value = {"/{lang}/motor-insurance/confirmation"}, method = RequestMethod.POST)
+	@RequestMapping(value = {"/{lang}/motor-insurance/confirmation"})
 	public ModelAndView confirmation(Model model, HttpServletRequest request) {
 		return MotorPageFlowControl.pageFlow("", model, request, UserRestURIConstants.PAGE_PROPERTIES_MOTOR_CONFIRMATION);
 	}
 	
-	@RequestMapping(value = {"/{lang}/motor-insurance/document-upload"}, method = RequestMethod.POST)
+	@RequestMapping(value = {"/{lang}/motor-insurance/document-upload"})
 	public ModelAndView documentupload(Model model, HttpServletRequest request) {
 		return MotorPageFlowControl.pageFlow("", model, request, UserRestURIConstants.PAGE_PROPERTIES_MOTOR_DOCUMENT_UPLOAD);
 	}
 	
-	@RequestMapping(value = {"/{lang}/motor-insurance/document-upload-confirmation"}, method = RequestMethod.POST)
+	@RequestMapping(value = {"/{lang}/motor-insurance/document-upload-confirmation"})
 	public ModelAndView documentuploadconfirmation(Model model, HttpServletRequest request) {
 		return MotorPageFlowControl.pageFlow("", model, request, UserRestURIConstants.PAGE_PROPERTIES_MOTOR_DOCUMENT_UPLOAD_CONFIRMATION);
 	}
 	
-	@RequestMapping(value = {"/{lang}/motor-insurance/document-upload-later"}, method = RequestMethod.POST)
+	@RequestMapping(value = {"/{lang}/motor-insurance/document-upload-later"})
 	public ModelAndView documentuploadlater(Model model, HttpServletRequest request) {
 		return MotorPageFlowControl.pageFlow("", model, request, UserRestURIConstants.PAGE_PROPERTIES_MOTOR_DOCUMENT_UPLOAD_LATER);
 	}
 	
-	@RequestMapping(value = {"/{lang}/motor-insurance/document-upload-later-confirmation"}, method = RequestMethod.POST)
+	@RequestMapping(value = {"/{lang}/motor-insurance/document-upload-later-confirmation"})
 	public ModelAndView documentuploadlaterconfirmation(Model model, HttpServletRequest request) {
 		return MotorPageFlowControl.pageFlow("", model, request, UserRestURIConstants.PAGE_PROPERTIES_MOTOR_DOCUMENT_UPLOAD_LATER_CONFIRMATION);
 	}
