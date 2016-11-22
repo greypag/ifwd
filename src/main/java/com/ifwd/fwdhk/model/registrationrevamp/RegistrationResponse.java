c708e930ab5568b5d3fa80ee6d8740aef0d6caea
John Huang <John.MX.Huang@pccw.com>
2016-11-22 15:25:38 +0800

    add registration and forgotpassword model

---
+++ src/main/java/com/ifwd/fwdhk/model/registrationrevamp/RegistrationResponse.java
@@ -0,0 +1,25 @@
+package com.ifwd.fwdhk.model.registrationrevamp;
+
+
+public class RegistrationResponse {
+	
+	private String userName;
+	private String message;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
