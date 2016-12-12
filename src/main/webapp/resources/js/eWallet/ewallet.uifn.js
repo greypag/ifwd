3409cd90c61c3c60317f70361a782497de4dd65f
Kitchen-Digital-1 <it@kitchen-digital.com>
2016-12-12 19:41:46 +0800

    eWallet - fix #4218

--- src/main/webapp/resources/js/eWallet/ewallet.uifn.js
+++ src/main/webapp/resources/js/eWallet/ewallet.uifn.js
@@ -322,7 +322,7 @@ function LinkupClass() {
 
 		});
 
-		this.bindResendOtpEvent();
+		this.initOtpHelper();
 	};
 
 	this.startLinkup = function(pid) {
@@ -497,7 +497,7 @@ function WithdrawClass(){
 			}
 		});
 
-		this.bindResendOtpEvent();
+		this.initOtpHelper();
 	};
 
 	this.restrictAmountInput = function (){
@@ -668,10 +668,16 @@ function BaseOtpHelper() {
 		this.popupDom.find(".ew_otp_input").val("");
 		this.popupDom.find("input[type=checkbox]").prop('checked', false);
 	};
-	this.bindResendOtpEvent = function(){
+	this.initOtpHelper = function(){
 		var that = this;
 		this.popupDom.find(".ew_link_resendOTP").on("click", function() {
 			that.resendOtp();
+		});
+
+		// click edge of input box (underline) will focus on text box
+		// underline is not part of input box.
+		this.popupDom.find(".ew_otp_wrapper").on("click", function (){
+			that.popupDom.find(".ew_otp_input").focus();
 		});
 	};
 }
